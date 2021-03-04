c4cc255efb2e9814efd3177fe5894905ef8c061e
farhanandriansyah <amandashakila10@gmail.com>
2021-03-04 12:47:21 +0700

    coba

---
+++ script.js
@@ -0,0 +1,14 @@
+
+	alert('Mau Bermain ?');
+	var namamu = prompt("Masukin nama kamu be : ")
+	alert('Halo ' + namamu + '!');
+
+	var yakin = confirm("Coba Klik OK");
+
+	if (yakin) {
+		document.write("I LOVE YOU SAYANG");
+	} else {
+		document.write("MAKASIH YA");
+	}
+
+
